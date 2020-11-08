1f9324181298a7e6a09cb01538b93e86447c5491
Ben <blscott@gmail.com>
2020-11-08 21:02:46 +1300

    added color grabber / bg color picker

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ let gridSize = 24;
 
 const container = document.querySelector('.grid-container');
 container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-
+let bgColor = '#ffffff'
 //create new grid items to fill the grid
 for (i = 0; i < gridSize ** 2; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-item');
     square.setAttribute('draggable', 'false');
-    square.style.backgroundColor = "#ffffff";
+    square.style.backgroundColor = bgColor;
     container.appendChild(square);
 };
+
+const gridItems = document.querySelectorAll(".grid-item");
 
 // toggle grid lines
 const gridButton = document.querySelector('#grid-btn');
@@ -23,11 +25,22 @@ gridButton.addEventListener('click', () => {
 // set default colour to black
 let ink = '#000000'; 
 
-//color picker
+//pen color picker
 const colorPicker = document.querySelector('#color-select')
 colorPicker.addEventListener("change", e => {
     ink = e.target.value;
-    console.log(ink);
+});
+
+// bg color picker
+// will not change the grid items that have the attribute data-inked = true
+const bgColorPicker = document.querySelector('#bg-color-select');
+bgColorPicker.addEventListener("input", e => {
+    for (let i = 0; i < gridItems.length; i++) {
+        if (!gridItems[i].dataset.inked) {
+            bgColor = e.target.value;
+            gridItems[i].style.backgroundColor = bgColor;
+        }       
+    } 
 });
 
 // toggle button colour when clicked
@@ -97,7 +110,27 @@ function adjust(RGBToHex, rgb, amount) {
         255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
 }
 
-// console.log(adjust(RGBToHex, "rgb(185, 0, 0)", -50 ))
+// eyedrop color grabbing tool
+const dropper = document.querySelector("#color-grabber");
+let grab = false;
+dropper.addEventListener('click', () => {
+    // when grab is true, all drawing is frozen until a color is selected
+    grab = true;   
+});
+
+//listen for clicks on all grid items when grab is true
+
+
+for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].addEventListener('click', e => {
+        if (grab) {
+            ink = e.target.style.backgroundColor;
+            colorPicker.value = RGBToHex(ink);
+            dropper.classList.remove('btn-on');
+            grab = false;   
+        }
+    });
+}
 
 
 
@@ -131,38 +164,55 @@ function randomColor() {
 
 
 // listen on grid squares for a click or click over and colour it in
-const gridItems = document.querySelectorAll(".grid-item");
+
 
 for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('mousedown', e => {
-        if (eraser) {
-            gridItems[i].style.backgroundColor = '#ffffff';
-        } else if (rainbow) {
-            gridItems[i].style.backgroundColor = randomColor();
-        } else if (shading) {
-            gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
-        } else if (lighten) {
-            gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
-        } else {
-            gridItems[i].style.backgroundColor = ink;
+        if (!grab) {
+            if (eraser) {
+                gridItems[i].style.backgroundColor = bgColor;
+                //data-inked = true means the background color change wont affect these elements
+                gridItems[i].removeAttribute('data-inked')
+            } else if (rainbow) {
+                gridItems[i].style.backgroundColor = randomColor();
+                gridItems[i].setAttribute('data-inked', 'true');
+            } else if (shading) {
+                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+                gridItems[i].setAttribute('data-inked', 'true');
+            } else if (lighten) {
+                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
+                gridItems[i].setAttribute('data-inked', 'true');
+            } else {
+                gridItems[i].style.backgroundColor = ink;
+                gridItems[i].setAttribute('data-inked', 'true');
+            }
         }
+        
         
         
     })
     // listen for a mouse over and change colour only if mouse button is pressed
     gridItems[i].addEventListener('mouseenter', e => {
         if (e.buttons > 0) {
-            if (eraser) {
-                gridItems[i].style.backgroundColor = '#ffffff';
-            } else if (rainbow) {
-                gridItems[i].style.backgroundColor = randomColor();
-            } else if (shading) {
-                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
-            } else if (lighten) {
-                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
-            } else {
-                gridItems[i].style.backgroundColor = ink;
+            if (!grab) {
+                if (eraser) {
+                    gridItems[i].style.backgroundColor = bgColor;
+                    gridItems[i].removeAttribute('data-inked');
+                } else if (rainbow) {
+                    gridItems[i].style.backgroundColor = randomColor();
+                    gridItems[i].setAttribute('data-inked', 'true');
+                } else if (shading) {
+                    gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+                    gridItems[i].setAttribute('data-inked', 'true');
+                } else if (lighten) {
+                    gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
+                    gridItems[i].setAttribute('data-inked', 'true');
+                } else {
+                    gridItems[i].style.backgroundColor = ink;
+                    gridItems[i].setAttribute('data-inked', 'true');
+                }
             }
+            
         };
     })
 };
