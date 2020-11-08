8be1fb9c60f8c992ced8392f4723df958d7c59a6
Ben <blscott@gmail.com>
2020-11-08 18:17:49 +1300

    lighting / shadding done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ for (i = 0; i < gridSize ** 2; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-item');
     square.setAttribute('draggable', 'false');
+    square.style.backgroundColor = "#ffffff";
     container.appendChild(square);
 };
 
@@ -49,67 +50,18 @@ shaderButton.addEventListener('click', () => {
     }
 });
 
+// lighten toggle 
+let lighten = false;
+const lightenButton = document.querySelector('#lighten-btn');
+lightenButton.addEventListener('click', () => {
+    if (lighten) {
+        lighten = false; 
+    } else {
+        lighten = true;
+    }
+});
+
 // shading function
-
-// convert hex color to hsl
-// function RGBToHSL(rgb) {
-//     let sep = rgb.indexOf(",") > -1 ? "," : " ";
-//     rgb = rgb.substr(4).split(")")[0].split(sep);
-  
-//     for (let R in rgb) {
-//       let r = rgb[R];
-//       if (r.indexOf("%") > -1)
-//         rgb[R] = Math.round(r.substr(0,r.length - 1) / 100 * 255);
-//     }
-  
-//     // Make r, g, and b fractions of 1
-//     let r = rgb[0] / 255,
-//         g = rgb[1] / 255,
-//         b = rgb[2] / 255;
-
-//     // Find greatest and smallest channel values
-//     let cmin = Math.min(r,g,b),
-//         cmax = Math.max(r,g,b),
-//         delta = cmax - cmin,
-//         h = 0,
-//         s = 0,
-//         l = 0;
-
-//     // Calculate hue
-//     // No difference
-//     if (delta == 0)
-//         h = 0;
-//     // Red is max
-//     else if (cmax == r)
-//         h = ((g - b) / delta) % 6;
-//     // Green is max
-//     else if (cmax == g)
-//         h = (b - r) / delta + 2;
-//     // Blue is max
-//     else
-//         h = (r - g) / delta + 4;
-
-//     h = Math.round(h * 60);
-  
-//     // Make negative hues positive behind 360°
-//     if (h < 0)
-//         h += 360;
-
-//     // Calculate lightness
-//     l = (cmax + cmin) / 2;
-
-//     // Calculate saturation
-//     s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    
-//     // Multiply l and s by 100
-//     s = +(s * 100).toFixed(1);
-//     l = +(l * 100).toFixed(1);
-
-//     return "hsl(" + h + "," + s + "%," + l + "%)";
-  
-    
-//   }
-
 
 function RGBToHex(rgb) {
     // Choose correct separator
@@ -129,21 +81,25 @@ function RGBToHex(rgb) {
       b = "0" + b;
       b = (+rgb[2]).toString(16);
 
-  if (r.length == 1)
-    r = "0" + r;
-  if (g.length == 1)
-    g = "0" + g;
-  if (b.length == 1)
-  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
     return "#" + r + g + b;
   }
 
+
 function adjust(RGBToHex, rgb, amount) {
     let color = RGBToHex(rgb);
-    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(
+        255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
 }
 
-console.log(RGBToHex('rgb(255,255,255)'))
+// console.log(adjust(RGBToHex, "rgb(185, 0, 0)", -50 ))
+
+
 
 // default eraser to false and listen for toggle
 let eraser = false;
@@ -180,11 +136,13 @@ const gridItems = document.querySelectorAll(".grid-item");
 for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('mousedown', e => {
         if (eraser) {
-            gridItems[i].style.backgroundColor = 'white';
+            gridItems[i].style.backgroundColor = '#ffffff';
         } else if (rainbow) {
             gridItems[i].style.backgroundColor = randomColor();
         } else if (shading) {
-            console.log(adjust(e.target.style.backgroundColor,-20));
+            gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+        } else if (lighten) {
+            gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
         } else {
             gridItems[i].style.backgroundColor = ink;
         }
@@ -195,9 +153,13 @@ for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener('mouseenter', e => {
         if (e.buttons > 0) {
             if (eraser) {
-                gridItems[i].style.backgroundColor = 'white';
+                gridItems[i].style.backgroundColor = '#ffffff';
             } else if (rainbow) {
                 gridItems[i].style.backgroundColor = randomColor();
+            } else if (shading) {
+                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+            } else if (lighten) {
+                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
             } else {
                 gridItems[i].style.backgroundColor = ink;
             }
