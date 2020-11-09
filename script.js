d9bf2ade0ec19b0933ee4f0efe10bf5fb935e7dd
Ben <blscott@gmail.com>
2020-11-09 17:28:34 +1300

    functionality done. added slider and clear

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 let gridSize = 24;
 
 const container = document.querySelector('.grid-container');
-container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
 let bgColor = '#ffffff'
 //create new grid items to fill the grid
-for (i = 0; i < gridSize ** 2; i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-item');
-    square.setAttribute('draggable', 'false');
-    square.style.backgroundColor = bgColor;
-    container.appendChild(square);
-};
+function createGrid(grid_size) {
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < gridSize ** 2; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        square.setAttribute('draggable', 'false');
+        square.style.backgroundColor = bgColor;
+        container.appendChild(square);
+    };
+
+    
+}
+
+createGrid(gridSize)
 
 const gridItems = document.querySelectorAll(".grid-item");
 
@@ -34,14 +42,7 @@ colorPicker.addEventListener("change", e => {
 // bg color picker
 // will not change the grid items that have the attribute data-inked = true
 const bgColorPicker = document.querySelector('#bg-color-select');
-bgColorPicker.addEventListener("input", e => {
-    for (let i = 0; i < gridItems.length; i++) {
-        if (!gridItems[i].dataset.inked) {
-            bgColor = e.target.value;
-            gridItems[i].style.backgroundColor = bgColor;
-        }       
-    } 
-});
+
 
 // toggle button colour when clicked
 const buttons = document.getElementsByTagName('button');
@@ -118,19 +119,7 @@ dropper.addEventListener('click', () => {
     grab = true;   
 });
 
-//listen for clicks on all grid items when grab is true
 
-
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener('click', e => {
-        if (grab) {
-            ink = e.target.style.backgroundColor;
-            colorPicker.value = RGBToHex(ink);
-            dropper.classList.remove('btn-on');
-            grab = false;   
-        }
-    });
-}
 
 
 
@@ -162,60 +151,128 @@ function randomColor() {
     return "#" + Math.floor(Math.random()*16777215).toString(16);
 }
 
+// slider
+function rangeSlider(value) {
+    document.getElementById('range-value').textContent = value;
+    gridSize = value;
+    deleteGrid()
+    createGrid()
+    listen()
+}
 
-// listen on grid squares for a click or click over and colour it in
+function rangeSliderValue(value) {
+    document.getElementById('range-value').textContent = value;
+    
+}
+
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeEventListener('mousedown', drawClick);
+        container.removeEventListener('mouseenter', drawClickHover);
+        container.lastChild = null;
+        container.removeChild(container.lastChild);
+    }
+}
 
 
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener('mousedown', e => {
+
+// clear grid
+const clearButton = document.querySelector('#clear-grid');
+function clearGrid() {
+    const gridItems = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].style.backgroundColor = bgColor;
+        gridItems[i].removeAttribute('data-inked');
+
+    }
+}
+clearButton.addEventListener('click', clearGrid);
+
+
+// draw on the grid when clicked
+function drawClick(e) {
+    if (!grab) {
+        if (eraser) {
+            e.target.style.backgroundColor = bgColor;
+            //data-inked = true means the background color change wont affect these elements
+            e.target.removeAttribute('data-inked')
+        } else if (rainbow) {
+            e.target.style.backgroundColor = randomColor();
+            e.target.setAttribute('data-inked', 'true');
+        } else if (shading) {
+            e.target.style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+            e.target.setAttribute('data-inked', 'true');
+        } else if (lighten) {
+            e.target.style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
+            e.target.setAttribute('data-inked', 'true');
+        } else {
+            e.target.style.backgroundColor = ink;
+            e.target.setAttribute('data-inked', 'true');
+        }
+    };
+}
+// draw when hovering over the grid with the mouse held down
+function drawClickHover(e) {
+    if (e.buttons > 0) {
         if (!grab) {
             if (eraser) {
-                gridItems[i].style.backgroundColor = bgColor;
-                //data-inked = true means the background color change wont affect these elements
-                gridItems[i].removeAttribute('data-inked')
+                e.target.style.backgroundColor = bgColor;
+                e.target.removeAttribute('data-inked');
             } else if (rainbow) {
-                gridItems[i].style.backgroundColor = randomColor();
-                gridItems[i].setAttribute('data-inked', 'true');
+                e.target.style.backgroundColor = randomColor();
+                e.target.setAttribute('data-inked', 'true');
             } else if (shading) {
-                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
-                gridItems[i].setAttribute('data-inked', 'true');
+                e.target.style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
+                e.target.setAttribute('data-inked', 'true');
             } else if (lighten) {
-                gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
-                gridItems[i].setAttribute('data-inked', 'true');
+                e.target.style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
+                e.target.setAttribute('data-inked', 'true');
             } else {
-                gridItems[i].style.backgroundColor = ink;
-                gridItems[i].setAttribute('data-inked', 'true');
+                e.target.style.backgroundColor = ink;
+                e.target.setAttribute('data-inked', 'true');
             }
         }
         
+    };
+}
+
+// listen for events
+function listen() {
+    const gridItems = document.querySelectorAll(".grid-item");
+    for (let i = 0; i < gridItems.length; i++) {
         
-        
-    })
-    // listen for a mouse over and change colour only if mouse button is pressed
-    gridItems[i].addEventListener('mouseenter', e => {
-        if (e.buttons > 0) {
-            if (!grab) {
-                if (eraser) {
-                    gridItems[i].style.backgroundColor = bgColor;
-                    gridItems[i].removeAttribute('data-inked');
-                } else if (rainbow) {
-                    gridItems[i].style.backgroundColor = randomColor();
-                    gridItems[i].setAttribute('data-inked', 'true');
-                } else if (shading) {
-                    gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,-15);
-                    gridItems[i].setAttribute('data-inked', 'true');
-                } else if (lighten) {
-                    gridItems[i].style.backgroundColor = adjust(RGBToHex,e.target.style.backgroundColor,+15);
-                    gridItems[i].setAttribute('data-inked', 'true');
-                } else {
-                    gridItems[i].style.backgroundColor = ink;
-                    gridItems[i].setAttribute('data-inked', 'true');
-                }
+        gridItems[i].addEventListener('mousedown', drawClick)
+        // listen for a mouse over and change colour only if mouse button is pressed
+        gridItems[i].addEventListener('mouseenter', drawClickHover)
+    };
+
+    //listen for clicks on all grid items when grab is true (color picker)
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener('click', e => {
+            if (grab) {
+                ink = e.target.style.backgroundColor;
+                colorPicker.value = RGBToHex(ink);
+                dropper.classList.remove('btn-on');
+                grab = false;   
             }
-            
-        };
-    })
-};
+        });
+    }
+
+    bgColorPicker.addEventListener("input", e => {
+        const gridItems = document.querySelectorAll(".grid-item");
+        for (let i = 0; i < gridItems.length; i++) {
+            if (!gridItems[i].dataset.inked) {
+                bgColor = e.target.value;
+                gridItems[i].style.backgroundColor = bgColor;
+            }       
+        } 
+    });
+}
+
+listen()
+
+
+
 
 
 
